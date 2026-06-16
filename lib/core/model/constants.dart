d802570857150464cb794d69f4af2f81d0627043
moneyfly004 <moneyfly004@users.noreply.github.com>
2026-06-16 16:18:52 +0800

    Publish installable MoneyFly releases

--- lib/core/model/constants.dart
+++ lib/core/model/constants.dart
@@ -3,11 +3,11 @@ import 'package:flutter/services.dart';
 import 'package:hiddify/utils/utils.dart';
 
 abstract class Constants {
-  static const appName = "Hiddify";
-  static const githubUrl = "https://github.com/hiddify/hiddify-next";
-  static const licenseUrl = "https://github.com/hiddify/hiddify-next?tab=License-1-ov-file#readme";
-  static const githubReleasesApiUrl = "https://api.github.com/repos/hiddify/hiddify-next/releases";
-  static const githubLatestReleaseUrl = "https://github.com/hiddify/hiddify-app/releases/latest";
+  static const appName = "MoneyFly";
+  static const githubUrl = "https://github.com/moneyfly004/cboard";
+  static const licenseUrl = "https://github.com/moneyfly004/cboard";
+  static const githubReleasesApiUrl = "https://api.github.com/repos/moneyfly004/cboard/releases";
+  static const githubLatestReleaseUrl = "https://github.com/moneyfly004/cboard/releases/latest";
   static const appCastUrl = "https://raw.githubusercontent.com/hiddify/hiddify-next/main/appcast.xml";
   static const telegramChannelUrl = "https://t.me/hiddify";
   static const privacyPolicyUrl = "https://hiddify.com/privacy-policy/";
